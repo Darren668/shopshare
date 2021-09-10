07a5959ab533b7595e33050b432874b567d67a75
Darren668 <xinhaojiegryx@163.com>
2021-09-10 22:49:06 +0100

    fix the bug of search function

--- src/main/java/pers/xinhaojie/shopshare/controller/SearchController.java
+++ src/main/java/pers/xinhaojie/shopshare/controller/SearchController.java
@@ -3,6 +3,7 @@ package pers.xinhaojie.shopshare.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +15,7 @@ import pers.xinhaojie.shopshare.entity.SharedOrder;
 import pers.xinhaojie.shopshare.entity.User;
 import pers.xinhaojie.shopshare.service.OrderService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -29,10 +31,20 @@ public class SearchController {
      * the order list by search
      * */
     @RequestMapping ("/search")
-    public String toMainPage(@RequestParam String searchTarget, @RequestParam(value = "pageNum",defaultValue = "1") Integer pageNum, Model model){
-
+    public String searchOrders(@RequestParam String searchTarget, @RequestParam(value = "pageNum",defaultValue = "1") Integer pageNum, Model model){
+        if(StringUtils.isBlank(searchTarget)){
+            return "redirect:/";
+        }
+        model.addAttribute("searchTarget",searchTarget);
+        //remove the + changed by springmvc
+        String[] targets = searchTarget.replaceAll(" +", " ").split(" ");
         PageHelper.startPage(pageNum, 3);
-        List<SharedOrder> orderList = orderService.list(new QueryWrapper<SharedOrder>().like("title", searchTarget).orderByDesc("create_time"));
+        QueryWrapper<SharedOrder> orderQueryWrapper = new QueryWrapper<>();
+        for(String key : targets){
+            orderQueryWrapper.like("title", key);
+        }
+        orderQueryWrapper.orderByDesc("create_time");
+        List<SharedOrder> orderList = orderService.list(orderQueryWrapper);
         PageInfo<SharedOrder> pageInfo = new PageInfo<>(orderList);
         model.addAttribute("pageInfo", pageInfo);
         return "search";
